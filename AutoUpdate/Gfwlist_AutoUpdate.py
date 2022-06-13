3292e48eef278b304c1188f08fa6b843b9e1770c
rwx9032 <61132134+rwx9032@users.noreply.github.com>
2022-06-13 23:50:37 +0800

    Update Gfwlist_AutoUpdate.py

--- AutoUpdate/Gfwlist_AutoUpdate.py
+++ AutoUpdate/Gfwlist_AutoUpdate.py
@@ -6,7 +6,7 @@ f2 = open('./gfwlist_AdguardHome.txt','w+')
 str1=r'server='
 str2=r'['
 str3=r'/127.0.0.1#5353'
-str4=r']tcp://223.6.6.6'
+str4=r']tls://1.1.1.1'
 for ss in f1.readlines():
     t1=re.sub(str1,str2,ss)
     t2=re.sub(str3,str4,t1)
