70d6290208da4c4f6b29bd0d5116c55ade2d2cc8
rwx9032 <61132134+rwx9032@users.noreply.github.com>
2022-06-13 23:39:47 +0800

    Update Gfwlist_AutoUpdate.py

--- AutoUpdate/Gfwlist_AutoUpdate.py
+++ AutoUpdate/Gfwlist_AutoUpdate.py
@@ -1,13 +1,13 @@
 import sys
 import re
  
-f1 = open('./gfwlist_decode.txt','r+')
+f1 = open('./gfwlist_dnsmasq.txt','r+')
 f2 = open('./gfwlist_AdguardHome.txt','w+')
 str1=r'server='
 str2=r'['
 str3=r'/114.114.114.114'
 str4=r']tcp://223.6.6.6'
-for ss in f1.readlines():
+for ss=4 in f1.readlines():
     t1=re.sub(str1,str2,ss)
     t2=re.sub(str3,str4,t1)
     f2.write(t2)
