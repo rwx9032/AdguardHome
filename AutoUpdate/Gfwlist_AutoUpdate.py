ae6175c805dbd0e1c00dad6b87055f0d2203a0cb
rwx9032 <61132134+rwx9032@users.noreply.github.com>
2022-06-13 23:43:33 +0800

    Update Gfwlist_AutoUpdate.py

--- AutoUpdate/Gfwlist_AutoUpdate.py
+++ AutoUpdate/Gfwlist_AutoUpdate.py
@@ -7,7 +7,7 @@ str1=r'server='
 str2=r'['
 str3=r'/114.114.114.114'
 str4=r']tcp://223.6.6.6'
-for ss=3 in f1.readlines():
+for ss in f1.readlines():
     t1=re.sub(str1,str2,ss)
     t2=re.sub(str3,str4,t1)
     f2.write(t2)
