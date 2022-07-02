08bc732219a533674f33fbe46fb613590f904227
rwx9032 <61132134+rwx9032@users.noreply.github.com>
2022-07-02 21:40:57 +0800

    Update ChinaDomainlist_AutoUpdate.py

--- AutoUpdate/ChinaDomainlist_AutoUpdate.py
+++ AutoUpdate/ChinaDomainlist_AutoUpdate.py
@@ -4,9 +4,9 @@ import re
 f1 = open('./accelerated-domains.china.conf','r+')
 f2 = open('./CNDomains_AdguardHome.txt','w+')
 str1=r'server='
-str2=r'[/'
+str2=r'['
 str3=r'/114.114.114.114'
-str4=r']tcp://223.6.6.6'
+str4=r'/]tcp://223.6.6.6'
 for ss in f1.readlines():
     t1=re.sub(str1,str2,ss)
     t2=re.sub(str3,str4,t1)
