0c908f170496d5a243eef0cb8c83e960121467d1
rwx9032 <61132134+rwx9032@users.noreply.github.com>
2025-02-08 19:53:08 +0000

    Clear outdated files and optimize files and variables naming

--- AutoUpdate/domain_list_converter.py
+++ AutoUpdate/domain_list_converter.py
@@ -1,26 +1,26 @@
 import sys
 import re
 
-def convert_list(input_file, output_file, source_type):
+def convert_list(input_filepath, output_filepath, source_type):
     """
     Convert domain lists to AdguardHome format
     source_type: 'gfw' or 'china'
     """
-    with open(input_file, 'r') as f1, open(output_file, 'w') as f2:
-        str1 = r'server='
-        str2 = r'['
+    with open(input_filepath, 'r') as source_file, open(output_filepath, 'w') as target_file:
+        pattern_server = r'server='
+        pattern_bracket = r'['
         
         if source_type == 'gfw':
-            str3 = r'/127.0.0.1#5353'
-            str4 = r'/]tls://1.1.1.1'
+            old_dns = r'/127.0.0.1#5353'
+            new_dns = r'/]tls://1.1.1.1'
         else:  # china
-            str3 = r'/114.114.114.114'
-            str4 = r'/]tcp://223.6.6.6'
+            old_dns = r'/114.114.114.114'
+            new_dns = r'/]tcp://223.6.6.6'
             
-        for line in f1:
-            t1 = re.sub(str1, str2, line)
-            t2 = re.sub(str3, str4, t1)
-            f2.write(t2)
+        for line in source_file:
+            converted_line = re.sub(pattern_server, pattern_bracket, line)
+            final_line = re.sub(old_dns, new_dns, converted_line)
+            target_file.write(final_line)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -28,7 +28,8 @@ if __name__ == "__main__":
         sys.exit(1)
         
     source_type = sys.argv[1]
-    input_file = sys.argv[2]
-    output_file = './gfwlist_AdguardHome.txt' if source_type == 'gfw' else './CNDomains_AdguardHome.txt'
+    input_filepath = sys.argv[2]
+    output_filename = 'gfwlist_AdguardHome.txt' if source_type == 'gfw' else 'CNDomains_AdguardHome.txt'
+    output_filepath = f'./{output_filename}'
     
-    convert_list(input_file, output_file, source_type)
+    convert_list(input_filepath, output_filepath, source_type)
